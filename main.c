faf24e9b93b8391f4fbd0b0ffda9738e605666b7
RayaneAll <9975@holbertonstudents.com>
2025-01-06 15:19:16 +0100

    Get user input

--- main.c
+++ main.c
@@ -8,6 +8,23 @@
 
 int main(void)
 {
-		printf("#cisfun$ ");
+		char *buffer = NULL;
+
+		size_t count = 0;
+		ssize_t nread;
+
+		while (1)
+		{
+			printf("#cisfun$ ");
+			nread = getline(&buffer, &count, stdin);
+
+			if (nread == -1)
+			{
+				perror("Exiting shell");
+				exit(1);
+			}
+			printf("%s", buffer);
+		}
+		free(buffer);
 		return (0);
 }
